4fa67dac0f8cb283ba84cffa2542d89427e14053
Esteban Rivera <estebanrivera654@gmail.com>
2025-09-08 08:52:52 -0600

    revision

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
 
 class Curso extends Model
 {
@@ -40,6 +42,36 @@ class Curso extends Model
                     ->withPivot('fecha_inscripcion', 'calificacion_final', 'estado')
                     ->withTimestamps();
     }
+
+    public function contenidos(): HasMany
+    {
+        return $this->hasMany(Contenido::class, 'curso_id');
+    }
+
+    public function actividades(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Actividad::class,   // Modelo destino
+            Contenido::class,   // Modelo intermedio
+            'curso_id',         // FK en contenidos que apunta a cursos
+            'contenido_id',     // FK en actividades que apunta a contenidos
+            'id',               // PK en cursos
+            'id'                // PK en contenidos
+        );
+    }
+
+    public function progreso(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            ProgresoCurso::class, // Modelo destino
+            Contenido::class,     // Modelo intermedio
+            'curso_id',           // FK en contenidos que apunta a cursos
+            'contenido_id',       // FK en progreso que apunta a contenidos
+            'id',                 // PK en cursos
+            'id'                  // PK en contenidos
+        );
+    }
+
 
     public function calificaciones(): HasMany
     {
